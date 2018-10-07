c4fb86b69c3c19aa1bd0d9fb238fa67b01de5980
Gaspar Jiménez <dgaspar@indra.es>
2018-10-07 16:59:04 +0200

    Se añaden maqueta inicial con estilos de material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MaterialModule} from './material.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+//Componente propios
+import { HelloWorld } from './primercomponente/primer.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PostDialogComponent } from './post-dialog/post-dialog.component';
+
+import { DataService } from './data/data.service';
+import { AuthService } from './auth.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HelloWorld, 
+    Componente2Component,
+	  WelcomeComponent,
+    DashboardComponent,
+	  PostDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,    
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService, 
+    AuthService],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    PostDialogComponent
+  ]
 })
 export class AppModule { }
